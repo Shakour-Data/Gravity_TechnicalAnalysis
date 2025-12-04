d784c21fdbc4c3dfa1c587d52c9c6e82044c9667
GravityWavesMl <GravityWavesManagement@gmail.com>
2025-12-04 14:27:56 +0330

    Commit for src/config.py

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # External TSE Database (Input Source)
 # This database contains the raw market data (price_data, sectors, etc.)
 # as defined in docs/inputs/DATABASE.md
-TSE_DB_FILE = os.path.join(BASE_DIR, 'data', 'tse_data.db')
+TSE_DB_FILE = r'E:\Shakour\MyProjects\GravityTseHisPrice\data\tse_data.db'
 
 # Internal Application Database (Output/Operational)
 # This database stores analysis results, user data, etc.
