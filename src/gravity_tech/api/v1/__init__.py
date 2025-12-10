c75b9bdf85d56d105077663451d460118c12393c
GravityWavesMl <GravityWavesManagement@gmail.com>
2025-12-10 20:09:25 +0330

    Update src/gravity_tech/api/v1/__init__.py

--- src/gravity_tech/api/v1/__init__.py
+++ src/gravity_tech/api/v1/__init__.py
@@ -13,12 +13,10 @@ from fastapi import APIRouter
 from gravity_tech.api.v1 import ml as ml_router
 from gravity_tech.api.v1 import tools as tools_router
 from gravity_tech.api.v1 import backtest as backtest_router
-from gravity_tech.api.v1 import db_explorer as db_router
 from gravity_tech.api.v1 import analysis as analysis_router
 
 router = APIRouter()
 router.include_router(ml_router.router)
 router.include_router(tools_router.router)
 router.include_router(backtest_router.router)
-router.include_router(db_router.router)
 router.include_router(analysis_router.router)
