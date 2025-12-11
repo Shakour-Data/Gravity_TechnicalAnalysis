823047f905f5f0741d12d91a1c2de5fd9b806126
GravityWavesMl <GravityWavesManagement@gmail.com>
2025-12-11 07:08:52 +0330

    database: update TSE data source module

--- src/gravity_tech/database/tse_data_source.py
+++ src/gravity_tech/database/tse_data_source.py
@@ -4,8 +4,9 @@ TSE Data Source Module
 Provides access to TSE market data.
 """
 
-from src.config import TSE_DB_FILE
-from src.database import TSEDatabaseConnector
+from config import TSE_DB_FILE
+
+from database import TSEDatabaseConnector
 
 # Create a singleton instance
 tse_data_source = TSEDatabaseConnector(TSE_DB_FILE)
